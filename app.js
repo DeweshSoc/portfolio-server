a3cda41bcb153bb8c2bcaad6f6501927491cf970
Dewesh Jha <deweshjha4@gmail.com>
2023-02-16 16:53:32 +0530

    implement post message functionality

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
 app.use("/",homeRoutes);
 
 
@@ -54,21 +56,21 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
 }
-// const MONGODB_URI = process.env.MONGODB_URI||process.env.MONGODB_URI_LOCAL;
-// mongoose
-//   .connect(MONGODB_URI, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//   })
-//   .then((result) => {
-//     console.log("Connected to Database");
-//     app.listen(port, (req) => {
-//       console.log("Server Up at", port);
-//     });
-//   })
-//   .catch((err) => console.log(err));
+const MONGODB_URI = process.env.MONGODB_URI||process.env.MONGODB_URI_LOCAL;
+mongoose
+  .connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((result) => {
+    console.log("Connected to Database");
+    app.listen(port, (req) => {
+      console.log("Server Up at", port);
+    });
+  })
+  .catch((err) => console.log(err));
 
-app.listen(port,req=>{
-  console.log("Server up at "+port);
-})
+// app.listen(port,req=>{
+//   console.log("Server up at "+port);
+// })
 
--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,43 @@
+const fs = require("fs");
+
+const Message = require("../models/message")
+
 exports.getHome = (req,res)=>{
   res.render("home");
 }
 
 exports.postMsg = (req,res)=>{
   console.log(req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const msg= req.body.message;
+
+  const newMsg =new Message({
+    name:name,
+    email:email,
+    message:msg
+  });
+
+  newMsg.save().then(()=>{
+    console.log("message successfully added!");
+    const toSend={
+      message:"success"
+    };
+    console.log(JSON.stringify(toSend));
+    res.status(200);
+    res.set('content-type','application/json');
+    res.send(JSON.stringify(toSend));
+  })
+  .catch(err=>{
+    console.log(err);
+    const toSend={
+      message:"error"
+    };
+    res.status(500);
+    res.send(JSON.stringify(toSend));
+  })
+
+
+
+
 }
